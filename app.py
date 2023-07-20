665175b02b555438f54cab88d17180fccdac6538
maxwarch <maximekuil@gmail.com>
2023-07-20 21:16:32 +0200

    todo colorscale

--- app.py
+++ app.py
@@ -10,13 +10,18 @@ css = open('./style.css', 'r').read()
 st.markdown('<style>' + css + '</style>', unsafe_allow_html=True)
 
 race_list, selected_race = render_sb()
-
 if selected_race != -1:
     s = race_list.loc[selected_race]
     df = load_xml(s.idrace)
     selected_coureurs = render_sb_multiselect_coureurs(df)
     st.title(s.text.rstrip() + ' [:movie_camera:](' + s.url + ')')
 
-    tab1, tab2 = st.tabs(["Par segment", "Global"])
-    render_tab1(tab1, df, selected_coureurs)
-    render_tab2(tab2, df, selected_coureurs)
+    if len(selected_coureurs) > 0:
+        tab1, tab2 = st.tabs(["Par segment", "Global"])
+        render_tab1(tab1, df, selected_coureurs)
+        render_tab2(tab2, df, selected_coureurs)
+    else:
+        st.write(':arrow_left: Choisissez des coureurs')
+else:
+    st.markdown('')
+    st.markdown('## :arrow_left: Choisissez une course')
--- views/tab1.py
+++ views/tab1.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import numpy as np
-import math
+import re
 
 graph_types = pd.DataFrame({
     'type': ['Vitesse/flotte', 'Distance parcourue/longueur du segment'],
@@ -21,7 +20,6 @@ def render_tab1(tab, df, selected_coureurs):
 			for fig in figs:
 				graph_index = 0
 				for col in cols:
-					col.markdown('<b><center>Leg' + str(index) + ' </center></b>', unsafe_allow_html=True)
 					col.plotly_chart(fig[graph_index], use_container_width=True)
 					graph_index += 1
 					index += 1
@@ -43,50 +41,128 @@ class DrawGraph:
 			selection = self.selection[self.selection.s_leg == int(index)]
 			min_dist = long_segment if np.min(general['s_distancesursegment']) > long_segment else np.min(general['s_distancesursegment'])
 
+			def dist_exceedtext(s_distancesursegment):
+				d = np.round(((s_distancesursegment * 100) / min_dist) - 100, 2)
+				for id, it in d.items():
+					if it > 0:
+						d.loc[id] = '+' + str(it) + '%' 
+					else:
+						d.loc[id] = ''
+				return d
+			
+			def speed_exceedtext(s_avgspeed):
+				d = np.round(((s_avgspeed * 100) / mean_speed) - 100, 2)
+				for id, it in d.items():
+					if it > 0:
+						d.loc[id] = '+' + str(it) + '%' 
+					else:
+						d.loc[id] = str(it) + '%' 
+				return d
+
+			selection = selection.assign(
+				nom_rank = lambda row: '#' + row.s_rankentersegment.astype(str) + ' ' + row.nom,
+				dist_exceed = lambda row: dist_exceedtext(row.s_distancesursegment),
+				speed_exceed = lambda row: speed_exceedtext(row.s_avgspeed),
+			)
+
 			range_x_dist= [min_dist - 20, np.max(selection['s_distancesursegment'])]
-			range_x_speed = [math.floor(np.min(general['s_avgspeed'])), np.max(general['s_avgspeed'])]
+			range_x_speed = [np.floor(np.min(general['s_avgspeed'])), np.max(general['s_avgspeed'])]
+
+			def SetColor(y):
+				y = re.findall(r"[-+]?(?:\d*\.*\d+)", y)
+				y = float(y[0])
+				if(y >= 10):
+					return "green"
+				if(y >= 5):
+					return "blue"
+				elif(y > 0):
+					return "yellow"
+				elif(y <= -10):
+					return "red"
+				elif(y <= 0):
+					return "orange"
 
 			data = [
 					go.Bar(
 							name="vitesse",
-							x=selection['s_avgspeed'],
-							y=selection.nom, 
-							text=selection['s_rankentersegment'],
+							x=selection.s_avgspeed,
+							y=selection.nom_rank,
+							text=selection.speed_exceed,
 							orientation='h',
 							xaxis='x',
-							offsetgroup=1
+							offsetgroup=1,
+							marker=dict(
+								color = list(map(SetColor, selection.speed_exceed)),
+							),
+							showlegend=False
 					),
 					go.Bar(
 							name="distance",
-							x=selection['s_distancesursegment'],
-							y=selection.nom, 
-							text=selection['s_rankentersegment'],
+							x=selection.s_distancesursegment,
+							y=selection.nom_rank, 
+							text=selection.dist_exceed,
 							orientation='h',
 							xaxis='x2',
 							offsetgroup=2
-					)
+					),
 			]
 
 			layout = go.Layout(
-				xaxis = go.layout.XAxis(title = 'Vitesse', range = range_x_speed, showline=True, ticklen=5, tickcolor='black'),
-				xaxis2 = go.layout.XAxis(title = 'Distance', side = 'top', overlaying = 'x', range = range_x_dist, showline=True, ticklen=5, tickcolor='black'),
+				xaxis = go.layout.XAxis(title = { 'text': 'Vitesse (knt)', 'standoff': 40 }, range = range_x_speed, showline=True, ticklen=5, tickcolor='black'),
+				xaxis2 = go.layout.XAxis(title = 'Distance (m)', side = 'top', overlaying = 'x', range = range_x_dist, showline=True, ticklen=5, tickcolor='black'),
+				title='Leg ' + str(index),
+				height=(len(selection) + 1) * (100 if len(selection) > 4 else 150),
+				
+				showlegend=True,
+				legend=dict(
+					orientation="h",
+					yanchor="bottom",
+					y=1.08,
+					xanchor="right",
+					x=1
+				),
 			)
 
 			fig = go.Figure(data, layout)
-			fig.add_vline(x=mean_speed, line_color='red', 
-										annotation_text="vmoy de la flotte " + str(round(mean_speed, 2)) + ' knt', 
-										annotation_position="bottom right",
-										annotation_bgcolor="white",
-										annotation_borderpad=3,
-									)
 			
-			fig.add_vline(x=long_segment, line_color='orange', 
-										annotation_text='long segment ' + str(round(long_segment, 2)) + ' m', 
-										annotation_position="top right",
-										annotation_bgcolor="white",
-										annotation_borderpad=3,
-										xref='x2'
-									)
+			fig.add_shape(type="line",
+				xref="x", yref="y",
+				x0=mean_speed, y0=-0.6, x1=mean_speed,
+				y1=len(selection) - 0.55,
+				line=dict(
+					color="red",
+					width=2,
+				),
+			)
+			fig.add_shape(type="line",
+				xref="x2", yref="y",
+				x0=long_segment, y0=-0.45, x1=long_segment,
+				y1=len(selection) - 0.5,
+				line=dict(
+					color="DarkOrange",
+					width=2,
+				),
+			)
+			fig.add_annotation(
+				x=mean_speed,
+				y= - 0.6,
+				text="vmoy " + str(np.round(mean_speed, 2)) + 'knt',
+				showarrow=False,
+				bgcolor="red",
+				borderpad=4,
+				font=dict(color="white")
+			)
+
+			fig.add_annotation(
+				x=long_segment,
+				y=len(selection) - 0.4,
+				text="segment " + str(np.round(long_segment, 2)) + 'm',
+				showarrow=False,
+				bgcolor="DarkOrange",
+				font=dict(color="white"),
+				borderpad=4,
+				xref='x2'
+			)
 			return fig
 
 		except KeyError:
