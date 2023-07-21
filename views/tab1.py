f69b7ee63de966c5c3668175b209072d63247452
maxwarch <maximekuil@gmail.com>
2023-07-21 15:09:59 +0200

    fix2 tab1

--- views/tab1.py
+++ views/tab1.py
@@ -35,131 +35,128 @@ class DrawGraph:
 		self.selection = selection
 
 	def add_graph(self, index):
-		try:
-			general = self.df[self.df.s_leg == int(index)]
-			#if general.empty:
-			#	return None
-			
-			mean_speed = np.nanmean(general['s_avgspeed'])
-			long_segment = np.nanmean(general['s_longueursegment'])
-			selection = self.selection[self.selection.s_leg == int(index)]
-			min_dist = long_segment if np.min(general['s_distancesursegment']) > long_segment else np.min(general['s_distancesursegment'])
+		general = self.df[self.df.s_leg == int(index)]
+		if general.empty:
+			return None
+		
 
-			def dist_exceedtext(s_distancesursegment):
-				d = np.round(((s_distancesursegment * 100) / min_dist) - 100, 2)
-				for id, it in d.items():
-					if it > 0:
-						d.loc[id] = '+' + str(it) + '%' 
-					else:
-						d.loc[id] = ''
-				return d
-			
-			def speed_exceedtext(s_avgspeed, format='percent'):
-				d = np.round(((s_avgspeed * 100) / mean_speed) - 100, 2)
-				for id, it in d.items():
-					if it > 0:
-						d.loc[id] = '+' + str(it) + '%' if format=='percent' else it
-					else:
-						d.loc[id] = str(it) + '%' if format=='percent' else it
-				return d
+		mean_speed = np.nanmean(general['s_avgspeed'])
+		long_segment = np.nanmean(general['s_longueursegment'])
+		selection = self.selection[self.selection.s_leg == int(index)]
+		min_dist = long_segment if np.min(general['s_distancesursegment']) > long_segment else np.min(general['s_distancesursegment'])
 
-			selection = selection.assign(
-				nom_rank = lambda row: '#' + row.s_rankentersegment.astype(str) + ' ' + row.nom,
-				dist_exceed = lambda row: dist_exceedtext(row.s_distancesursegment),
-				speed_exceed = lambda row: speed_exceedtext(row.s_avgspeed),
-				speed_exceed_float = lambda row: speed_exceedtext(row.s_avgspeed, 'float'),
-			)
+		def dist_exceedtext(s_distancesursegment):
+			d = np.round(((s_distancesursegment * 100) / min_dist) - 100, 2)
+			for id, it in d.items():
+				if it > 0:
+					d.loc[id] = '+' + str(it) + '%' 
+				else:
+					d.loc[id] = ''
+			return d
+		
+		def speed_exceedtext(s_avgspeed, format='percent'):
+			d = np.round(((s_avgspeed * 100) / mean_speed) - 100, 2)
+			for id, it in d.items():
+				if it > 0:
+					d.loc[id] = '+' + str(it) + '%' if format=='percent' else it
+				else:
+					d.loc[id] = str(it) + '%' if format=='percent' else it
+			return d
 
-			range_x_dist= [min_dist - 20, np.max(selection['s_distancesursegment'])]
-			range_x_speed = [np.floor(np.min(general['s_avgspeed'])), np.max(general['s_avgspeed'])]
+		selection = selection.assign(
+			nom_rank = lambda row: '#' + row.s_rankentersegment.astype(str) + ' ' + row.nom,
+			dist_exceed = lambda row: dist_exceedtext(row.s_distancesursegment),
+			speed_exceed = lambda row: speed_exceedtext(row.s_avgspeed),
+			speed_exceed_float = lambda row: speed_exceedtext(row.s_avgspeed, 'float'),
+		)
 
-			data = [
-					go.Bar(
-							name="vitesse",
-							x=selection.s_avgspeed,
-							y=selection.nom_rank,
-							text=selection.speed_exceed,
-							orientation='h',
-							xaxis='x',
-							offsetgroup=1,
-							marker=dict(
-								color = list(map(set_graph_color, selection.speed_exceed_float)),
-							),
-							showlegend=False
-					),
-					go.Bar(
-							name="distance",
-							x=selection.s_distancesursegment,
-							y=selection.nom_rank, 
-							text=selection.dist_exceed,
-							orientation='h',
-							xaxis='x2',
-							offsetgroup=2
-					),
-			]
+		range_x_dist= [min_dist - 20, np.max(selection['s_distancesursegment'])]
+		range_x_speed = [np.floor(np.min(general['s_avgspeed'])), np.max(general['s_avgspeed'])]
 
-			layout = go.Layout(
-				xaxis = go.layout.XAxis(title = { 'text': 'Vitesse (knt)', 'standoff': 40 }, range = range_x_speed, showline=True, ticklen=5, tickcolor='black'),
-				xaxis2 = go.layout.XAxis(title = 'Distance (m)', side = 'top', overlaying = 'x', range = range_x_dist, showline=True, ticklen=5, tickcolor='black'),
-				title='Leg ' + str(index),
-				height=(len(selection) + 1) * (100 if len(selection) > 4 else 150),
-				
-				showlegend=True,
-				legend=dict(
-					orientation="h",
-					yanchor="bottom",
-					xanchor="left",
-					y=0,
-					x=0,
+		data = [
+				go.Bar(
+						name="vitesse",
+						x=selection.s_avgspeed,
+						y=selection.nom_rank,
+						text=selection.speed_exceed,
+						orientation='h',
+						xaxis='x',
+						offsetgroup=1,
+						marker=dict(
+							color = list(map(set_graph_color, selection.speed_exceed_float)),
+						),
+						showlegend=False
 				),
-			)
+				go.Bar(
+						name="distance",
+						x=selection.s_distancesursegment,
+						y=selection.nom_rank, 
+						text=selection.dist_exceed,
+						orientation='h',
+						xaxis='x2',
+						offsetgroup=2
+				),
+		]
 
-			colorbar = create_colorbar()
-			fig = go.Figure(data + colorbar, layout)
+		layout = go.Layout(
+			xaxis = go.layout.XAxis(title = { 'text': 'Vitesse (knt)', 'standoff': 40 }, range = range_x_speed, showline=True, ticklen=5, tickcolor='black'),
+			xaxis2 = go.layout.XAxis(title = 'Distance (m)', side = 'top', overlaying = 'x', range = range_x_dist, showline=True, ticklen=5, tickcolor='black'),
+			title='Leg ' + str(index),
+			height=(len(selection) + 1) * (100 if len(selection) > 4 else 150),
 			
-			fig.add_shape(
-				type="line",
-				xref="x",
-				x0=mean_speed, y0=-1.3, x1=mean_speed, y1=len(selection) - 0.55,
-				line=dict(
-					color="red",
-					width=2,
-				),
-			)
-			fig.add_shape(
-				type="line",
-				xref="x2",
-				x0=long_segment, y0=-0.45, x1=long_segment, y1=len(selection) - 0.5,
-				line=dict(
-					color="DarkOrange",
-					width=2,
-				),
-			)
-			fig.add_annotation(
-				x=mean_speed,
-				y= -0.63,
-				text="vmoy " + str(np.round(mean_speed, 2)) + 'knt',
-				showarrow=False,
-				bgcolor="red",
-				borderpad=4,
-				font=dict(color="white")
-			)
+			showlegend=True,
+			legend=dict(
+				orientation="h",
+				yanchor="bottom",
+				xanchor="left",
+				y=0,
+				x=0,
+			),
+		)
 
-			fig.add_annotation(
-				x=long_segment,
-				y=len(selection) - 0.4,
-				text="segment " + str(np.round(long_segment, 2)) + 'm',
-				showarrow=False,
-				bgcolor="DarkOrange",
-				font=dict(color="white"),
-				borderpad=4,
-				xref='x2'
-			)
+		colorbar = create_colorbar()
+		fig = go.Figure(data + colorbar, layout)
+		
+		fig.add_shape(
+			type="line",
+			xref="x",
+			x0=mean_speed, y0=-1.3, x1=mean_speed, y1=len(selection) - 0.55,
+			line=dict(
+				color="red",
+				width=2,
+			),
+		)
+		fig.add_shape(
+			type="line",
+			xref="x2",
+			x0=long_segment, y0=-0.45, x1=long_segment, y1=len(selection) - 0.5,
+			line=dict(
+				color="DarkOrange",
+				width=2,
+			),
+		)
+		fig.add_annotation(
+			x=mean_speed,
+			y= -0.63,
+			text="vmoy " + str(np.round(mean_speed, 2)) + 'knt',
+			showarrow=False,
+			bgcolor="red",
+			borderpad=4,
+			font=dict(color="white")
+		)
 
-			return fig
+		fig.add_annotation(
+			x=long_segment,
+			y=len(selection) - 0.4,
+			text="segment " + str(np.round(long_segment, 2)) + 'm',
+			showarrow=False,
+			bgcolor="DarkOrange",
+			font=dict(color="white"),
+			borderpad=4,
+			xref='x2'
+		)
 
-		except KeyError:
-				return
+		return fig
 
 	def change_graph(self, fig_cols=2):
 		result = []
