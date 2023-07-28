042f442b34410fad6333b4983b8ffacaaf0bd400
maxwarch <maximekuil@gmail.com>
2023-07-28 18:30:24 +0200

    start tab2

--- views/tab2.py
+++ views/tab2.py
@@ -1,9 +1,25 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
-import plotly.express as px
+
 import numpy as np
 
-def render_tab2(tab, df, selected_coureurs):
-    with tab:
-        st.write('En cours...')
+def render_tab2(tab, df):
+	graph = DrawGraph(df)
+	try:
+		with tab:
+			tab.pyplot(graph.add_graph())
+	except KeyError:
+		st.error('Pas de donnée valable')
+
+
+
+class DrawGraph:
+	def __init__(self, df):
+		self.df = df
+
+	def add_graph(self, index):
+		general = self.df[self.df.s_leg == self.df.s_leg.max()]
+		if general.empty:
+			return None
+		
+        print(general)
